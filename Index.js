047d8e114616ef04c20602ef930c45215faeb9d5
Peach-Iced-Tea <peach.iced.tea.01@gmail.com>
2023-05-20 23:35:43 +0200

    Fully functioning code able to fetch all images In total over 4,5k images or about 12-13 years of 1 image per day

--- Index.js
+++ Index.js
@@ -3,20 +3,47 @@ import fs from "fs";
 
 const sleep = s => new Promise(r => setTimeout(r, s*1000))
 
+async function renderImages(page){
+  let i = 1;
+  let loop = true;
+  while (loop == true){
+    try {
+      await page.click('#trending.col-md-12');
+      await sleep(2);
+      console.log(24+i*24, " Images have been rendered")
+      i+=1
+    } catch (err) {
+        console.log(err.message)
+        loop = false;
+    }
+  }
+}
+
 async function accessImage(page, extention, index){
   let imgUrl = "https://www.nasa.gov" + extention
   let imgPath = "./Images/nasaIMG_"+ index + ".png"
+  let folderName = './Images';
+
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 
   console.log(imgUrl)
   page.goto(imgUrl, {waitUntil:"domcontentloaded"});
 
   await sleep(2)
 
-  const element = await page.$('body > img');        // declare a variable with an ElementHandle
+  const element = await page.$('body > img');
   await element.screenshot({path: imgPath})
 }
+
 async function scrape(url) {
   let imgId = 1
+
     const browser = await puppeteer.launch({
       headless: false,
       defaultViewport: null,
@@ -32,6 +59,8 @@ async function scrape(url) {
 
     console.log("Page rendered");
 
+    await renderImages(page);
+
     let images = await page.evaluate(()=>{
       const pageData = document.querySelectorAll("div.row.gallery-container > div#gallery-list > div.gallery-card.ember-view");
 
@@ -46,7 +75,7 @@ async function scrape(url) {
     for (let image of images) {
       await accessImage(page, image["imgSrc"], imgId);
       imgId += 1
-    }
+    };
 
     await browser.close();
 };
